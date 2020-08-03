a9fec48bad6b862826d4178ce6fe19392ce2aa69
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2020-08-03 16:46:45 +0430

    add SAN model.

---
+++ Code/SAN/SAN_LSTM_model.py
@@ -0,0 +1,50 @@
+from constants import *
+from image_layer import *
+from question_layer_LSTM import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import, Dense, Input
+
+
+def SAN_LSTM(num_classes, dropout_rate, num_words, embedding_dim, attention_dim):
+
+    qs_input = Input(shape=(SEQ_LENGTH,))
+    img_input = Input(shape=(512, 14, 14))
+
+    image_embed = image_layer()(img_input)
+    ques_embed = question_layer_LSTM(num_words,
+                                     embedding_dim,
+                                     dropout_rate,
+                                     SEQ_LENGTH)(qs_input)
+
+    att = attention_layer(attention_dim)([image_embed, ques_embed])
+    att = attention_layer(attention_dim)([image_embed, att])
+
+    output = Dense(num_classes, activation='softmax')(att)
+
+    model = Model(inputs=[qs_input, img_input], outputs=output)
+
+    return model
+
+
+def Train():
+
+    checkpoint = ModelCheckpoint('model_SAN_LSTM_2.h5', save_best_only=True)
+
+    model = SAN_LSTM(num_classes,
+                     DROPOUT,
+                     VOCAB_SIZE,
+                     EMBEDDING_DIM,
+                     ATTENTION_DIM)
+
+    model.compile(optimizer='sgd',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.summary()
+    history = model.fit([train_X_seqs, train_X_ims],
+                        train_Y,
+                        epochs=EPOCHS,
+                        batch_size=BATCH_SIZE,
+                        validation_data=([val_X_seqs, val_X_ims], val_Y),
+                        callbacks=[checkpoint])
+
+    return history
