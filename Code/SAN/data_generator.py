46c9f08cd5dc8071d75b78fc82f4a4e9ddf8a6af
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2020-08-06 17:48:08 +0430

    add logger to data_generator.py

--- Code/SAN/data_generator.py
+++ Code/SAN/data_generator.py
@@ -3,6 +3,17 @@ from constants import *
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.utils import to_categorical
 
+# logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('data_generator.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
 
 class DataGenerator(Sequence):
 
@@ -19,10 +30,10 @@ class DataGenerator(Sequence):
 
     def __len__(self):
         """
-        Denotes the number of batches per epoch
+        Denotes the number of batches per epoch.
 
         """
-        return int(np.ceil(len(self.questions) / self.batch_size))
+        return int(np.floor(len(self.questions) / self.batch_size))
 
     def __getitem__(self, index):
         """
@@ -35,6 +46,7 @@ class DataGenerator(Sequence):
         # Generate data
         [x_seqs, x_ims], y = self.__data_generation(indexes)
 
+        logger.info("get %i/%i batches of data." % (index+1, self.__len__))
         return [x_seqs, x_ims], y
 
     def on_epoch_end(self):
@@ -42,20 +54,20 @@ class DataGenerator(Sequence):
         Updates indexes after each epoch'
         """
 
-        self.indexes = np.arange(len(self.list_IDs))
+        self.indexes = np.arange(len(self.questions))
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
+
+        logger.info("end of epoch and shuffle data.")
 
     def __data_generation(self, indexes):
         """
         Generates data containing batch_size samples
         """
-
         x_seqs = np.empty((self.batch_size, SEQ_LENGTH))
-        x_ims = np.empty((512, 14, 14))
+        x_ims = np.empty((self.batch_size, 512, 14, 14))
         y = np.empty((self.batch_size), dtype=int)
 
-        # Generate data
         for i in indexes:
             # Store sample
             x_seqs[i] = self.questions[i]
@@ -64,4 +76,5 @@ class DataGenerator(Sequence):
             # Store class
             y[i] = self.answers[i]
 
+        logger.info("create one batch of data.")
         return [x_seqs, x_ims], to_categorical(y, num_classes=self.n_classes)
