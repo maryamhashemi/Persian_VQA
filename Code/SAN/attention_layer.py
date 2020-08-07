2a018e995dc209424be9ada21b1bfd8704e5ecd8
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2020-08-07 17:52:11 +0430

    update.

--- Code/SAN/attention_layer.py
+++ Code/SAN/attention_layer.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 
 
 class attention_layer(Model):
