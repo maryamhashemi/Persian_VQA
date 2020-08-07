26d522e1f167c64456d7227666fd2d8f3958e7ef
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2020-08-07 16:56:57 +0430

    update prepare_QA.py and prepare_generator.py.

--- Code/SAN/prepare_QA.py
+++ Code/SAN/prepare_QA.py
@@ -1,4 +1,6 @@
+import heapq
 from constants import *
+from sklearn.preprocessing import LabelBinarizer
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -24,11 +26,11 @@ def merge_QA(questions, annotations):
     Arguments:
     questions -- a dictionary that consists of image_id, question and question_id.
     annotations -- a dictionary that consists of answer_type, answers, image_id, 
-                   multiple_choice_answer, question_id and question_type
+                   multiple_choice_answer, question_id and question_type.
 
     Return:
     QA -- a dataframe with 7 columns that are image_id, question, question_id, answer_type, answers, 
-            multiple_choice_answer and question_type
+            multiple_choice_answer and question_type.
 
     """
     questions = pd.DataFrame(questions)
@@ -43,7 +45,7 @@ def merge_QA(questions, annotations):
 
 def get_QA(questions_path, annotations_path):
     """
-    load question and annotation files and merge their contents and return the merged data as a dataframe
+    load question and annotation files and merge their contents and return the merged data as a dataframe.
 
     Arguments:
     questions_path -- a string that shows questions file path.
@@ -62,8 +64,108 @@ def get_QA(questions_path, annotations_path):
     logger.info("load annotations.")
 
     QA = merge_QA(questions, annotations)
-
+    logger.info("total number of data is %i ." % (len(QA)))
     return QA
+
+
+def get_answer_frequency(answers):
+    """
+    calculate the frequency of each answer and return a dictionary of all answer frequencies.
+
+    Arguments:
+    answers -- a string vector that consists of answers.
+
+    Return:
+    answer_frequency -- a dictionary that maps answer to frequency.
+
+    """
+    answer_frequency = {}
+
+    for answer in answers:
+        if(answer_frequency.get(answer, -1) > 0):
+            answer_frequency[answer] += 1
+        else:
+            answer_frequency[answer] = 1
+
+    logger.info("calculate all answer frequencies.")
+    return answer_frequency
+
+
+def get_k_top_answers(k, answer_frequency):
+    """
+    choose the top k most frequent answers.
+
+    Arguments:
+    k -- an integer that indicates the number of answers(classes).
+    answer_frequency -- a dictionary that maps answer to frequency.
+
+    Return:
+    k_frequent_answers -- a dictionary that indicates top k most frequent answers.
+
+    """
+    k_frequent_answers = heapq.nlargest(k,
+                                        answer_frequency,
+                                        key=answer_frequency.get)
+
+    logger.info("choose the top k most frequent answers.")
+    return k_frequent_answers
+
+
+def filter_questions(k_frequent_answers, data):
+    """
+    remove the questions that their answers don't exist in top k frequent answers.
+
+    Arguments:
+    k_frequent_answers -- a dictionary that indicates top k most frequent answers.
+    data -- a dataframe that consists of questions and answers.
+
+    Return:
+    filtered_data -- a dataframe that filtered by answers.
+
+    """
+    data['multiple_choice_answer'] = data['multiple_choice_answer'].apply(
+        lambda x: x if x in k_frequent_answers else '')
+
+    filtered_data = data[data['multiple_choice_answer'].apply(
+        lambda x:len(x) > 0)]
+
+    logger.info("remove the questions from data.")
+    return filtered_data
+
+
+def get_train_val_label(train_data, val_data):
+    """
+    convert answers of training and validation questions to one hot vector.
+
+    Arguments:
+    train_data -- a dataframe that consists of training questions and answers.
+    val_data -- a dataframe that consists of validation questions and answers.
+
+    Return:
+    ans_vocab -- a dictionary that maps answers to an int number(label).
+    train_Y -- a numpy array that has shape of (number of training example, NUM_CLASSES).
+    val_Y -- a numpy array that has shape of (number of validation example, NUM_CLASSES).
+
+    """
+    label_encoder = LabelBinarizer()
+
+    train_Y = label_encoder.fit_transform(
+        train_data['multiple_choice_answer'].apply(lambda x: x).values)
+    logger.info("get train labels.")
+
+    val_Y = label_encoder.transform(
+        val_data['multiple_choice_answer'].apply(lambda x: x).values)
+    logger.info("get val labels.")
+
+    ans_vocab = {l: i for i, l in enumerate(label_encoder.classes_)}
+
+    logger.info("Number of clasess: ", len(ans_vocab))
+    logger.info("Shape of Answer Vectors in training Data: " +
+                str(train_Y.shape))
+    logger.info("Shape of Answer Vectors in validation Data: " +
+                str(train_Y.shape))
+
+    return ans_vocab, train_Y, val_Y
 
 
 def preprocess_question(train_qs, val_qs, test_qs):
--- Code/SAN/prepare_generator.py
+++ Code/SAN/prepare_generator.py
@@ -7,19 +7,24 @@ train_data = get_QA(GOOGLE_QUESTION_TRAIN_PATH, GOOGLE_ANNOTATION_TRAIN_PATH)
 val_data = get_QA(GOOGLE_QUESTION_VAL_PATH, GOOGLE_ANNOTATION_VAL_PATH)
 test_data = get_QA(GOOGLE_QUESTION_TEST_PATH, GOOGLE_ANNOTATION_TEST_PATH)
 
-train_questions = train_data["question"]
-val_questions = val_data["question"]
-test_question = test_data["question"]
+# get questions
+train_questions = train_data["question"].values
+val_questions = val_data["question"].values
+test_question = test_data["question"].values
 
+# apply preprocessing on questions
 train_seqs, val_seqs, test_seqs = preprocess_question(
     train_questions, val_questions, test_question)
 
-train_image_ids = train_data["image_id"]
-val_image_ids = val_image_ids["image_id"]
+# get iamge ids
+train_image_ids = train_data["image_id"].values
+val_image_ids = val_image_ids["image_id"].values
 
+# get image feature paths
 train_image_path = get_train_img_feature_paths()
 val_image_path = get_val_img_feature_paths()
 
+# get answers
 train_answers = None  # not yet implemented
 val_answers = None  # not yet implemented
 
