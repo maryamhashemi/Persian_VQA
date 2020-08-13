5406f88f9d3d23199e423b28a387759e6133a156
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2020-08-13 13:46:49 +0430

    Experiment 2.

--- Code/SAN/question_layer_LSTM.py
+++ Code/SAN/question_layer_LSTM.py
@@ -14,8 +14,10 @@ class question_layer_LSTM(Model):
                                    weights=[embedding_matrix],
                                    trainable=False)
         self.lstm1 = LSTM(units=1024, return_sequences=True)
+        self.batch1 = BatchNormalization(center=False, scale=False)
         self.dropout1 = Dropout(dropout_rate)
         self.lstm2 = LSTM(units=1024, return_sequences=False)
+        self.batch2 = BatchNormalization(center=False, scale=False)
         self.dropout2 = Dropout(dropout_rate)
         self.dense = Dense(1024, activation='tanh')
         self.batch = BatchNormalization(center=False, scale=False)
@@ -26,12 +28,12 @@ class question_layer_LSTM(Model):
 
         # (N, SEQ_LENGTH, embedding_dim) -> (N, SEQ_LENGTH, 512)
         x = self.lstm1(x)
-        x = self.batch(x)
+        x = self.batch1(x)
         x = self.dropout1(x)
 
         # (N, SEQ_LENGTH, 512) -> (N, 512)
         x = self.lstm2(x)
-        x = self.batch(x)
+        x = self.batch2(x)
         x = self.dropout2(x)
 
         # (N, 512) -> (N * 1024)
